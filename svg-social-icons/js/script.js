dc013b07e7884d2670aa37b149d362f2a4738090
Jorge Aznar <jorgeatgu@gmail.com>
2015-05-08 22:32:34 +0200

    desactivando animaciones

--- svg-social-icons/js/script.js
+++ svg-social-icons/js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
-	var $images = $('svg');
+	// var $images = $('svg');
 
-	$images.velocity({ scale: 2 },{
-      duration: 1000,
-      loop: 1,
-      easing: "spring"
-    });
+	// $images.velocity({ scale: 2 },{
+ //      duration: 1000,
+ //      loop: 1,
+ //      easing: "spring"
+ //    });
 
-	$('button').click(function(){
-	    $("svg").delay(199).velocity({ scale: 0}, { duration: 500, easing: "easeInCirc"});
-	});
+	// $('button').click(function(){
+	//     $("svg").delay(199).velocity({ scale: 0}, { duration: 500, easing: "easeInCirc"});
+	// });
+
+
+
+	// $('svg').hover(function(){
+	//     $(this).stop().velocity({ scale: 20}, { duration: 100, easing: "easeInCirc"});
+ //    });
 });
