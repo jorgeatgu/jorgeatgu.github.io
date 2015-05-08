4c8db6c1208ba375c9e361bea8de8c6504646b5f
Jorge Aznar <jorgeatgu@gmail.com>
2015-05-08 21:16:22 +0200

    modificando el script

--- svg-social-icons/js/script.js
+++ svg-social-icons/js/script.js
@@ -9,14 +9,8 @@ $(document).ready(function() {
     });
 
 	$('button').click(function(){
-	    $("#segunda-imagen,#tercera-imagen,#cuarta-imagen,#quinta-imagen,#sexta-imagen")
+	    $("$images")
 	      .delay(199)
 	      .velocity({ scale: 0}, { duration: 500, easing: "easeInCirc"})
-      $("#primera-imagen")
-        .delay(400)
-        .velocity({scale: 2, translateX: "100px"}, { duration: 2000, easing: [ 400, 20 ]})
-       $('#primer-texto')
-       	.delay(600)
-       	.velocity({translateX: "600px", translateY: "-180px", opacity: "1"}, { duration:1000, easing: "spring"})
 	});
 });
