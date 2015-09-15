16ad0bb9d70834484e19eef3d0fe7db0891809a1
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2015-09-15 23:15:13 +0200

    terminando animacion

--- octuweb-velocityJS/js/animacion.js
+++ octuweb-velocityJS/js/animacion.js
@@ -50,23 +50,8 @@ $("#uno").delay(200)
                                         .velocity({
                                             strokeWidth: 1500,
                                             complete: function() {
-                                                $("#cinco")
-                                                    .delay(200)
-                                                    .velocity({
-                                                        x2: 0,
-                                                        y2: 600
-                                                    }, {
-                                                        duration: 1000,
-                                                        easing: "spring"
-                                                    })
-                                                    .delay(50)
-                                                    .velocity({
-                                                        strokeWidth: 1500,
-                                                        complete: function() {
-                                                            $("#contenedor-logo")
-                                                                .show(300)
-                                                        }
-                                                    });
+                                                $("#contenedor-logo")
+                                                    .show(300)
                                             }
                                         });
                                 }
@@ -76,13 +61,34 @@ $("#uno").delay(200)
         }
     });
 
-$(".logo-boton").click(function() {
+$(".logo-boton").hover(function() {
     $("#logo").velocity({
         rotateZ: 360,
         scale: 1.5
     }, {
-        loop: 2
+        loop: 1
     })
-    // $("#letras").hide();
-    $("rect").velocity({ height: 75, width: 100 });
+});
+
+$(".logo-boton").click(function(){
+    $("#cuatro, #tres, #dos, #uno").velocity({
+        strokeWidth: 1,
+        x2: 0
+    }, {
+        duration: 750
+    })
+    .delay(25)
+    .velocity({
+        y2:0,
+        x1:0
+    }, {
+        duration: 500
+    });
+    $("#contenedor-logo").delay(1500)
+    .velocity({
+        scale: 0
+    }, {
+        duration: 500,
+        easing: "spring"
+    })
 });
