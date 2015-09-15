7accbc1a0a2f3edd18bc09347af625db6335a4e6
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2015-09-15 18:26:50 +0200

    completando la primera parte de la animacion

--- octuweb-velocityJS/js/animacion.js
+++ octuweb-velocityJS/js/animacion.js
@@ -1,8 +1,11 @@
+/* Animacion para el artículo de "Animando SVG con VelocityJS" por Jorge Aznar :) http://jorgeatgu.com
+========================================================================== */
+
 $("#uno").delay(200)
     .velocity({
         y2: 600
     }, {
-        duration: 1500,
+        duration: 1000,
         easing: "spring"
     })
     .delay(150)
@@ -11,11 +14,11 @@ $("#uno").delay(200)
         complete: function() {
             $(function() {
                 $("#dos")
-                    .delay(1000)
+                    .delay(200)
                     .velocity({
                         x2: 0
                     }, {
-                        duration: 1500,
+                        duration: 1000,
                         easing: "swing"
                     })
                     .delay(50)
@@ -24,12 +27,12 @@ $("#uno").delay(200)
                         complete: function() {
                             $(function() {
                                 $("#tres")
-                                    .delay(1000)
+                                    .delay(200)
                                     .velocity({
                                         x2: 1200,
                                         y2: 600
                                     }, {
-                                        duration: 1500,
+                                        duration: 1000,
                                         easing: "spring"
                                     })
                                     .delay(50)
@@ -38,12 +41,12 @@ $("#uno").delay(200)
                                         complete: function() {
                                             $(function() {
                                                 $("#cuatro")
-                                                    .delay(1000)
+                                                    .delay(200)
                                                     .velocity({
                                                         x2: 1200,
                                                         y2: 0
                                                     }, {
-                                                        duration: 1500,
+                                                        duration: 1000,
                                                         easing: "swing"
                                                     })
                                                     .delay(50)
@@ -52,12 +55,12 @@ $("#uno").delay(200)
                                                         complete: function() {
                                                             $(function() {
                                                                 $("#cinco")
-                                                                    .delay(2000)
+                                                                    .delay(200)
                                                                     .velocity({
                                                                         x2: 0,
                                                                         y2: 600
                                                                     }, {
-                                                                        duration: 1500,
+                                                                        duration: 1000,
                                                                         easing: "spring"
                                                                     })
                                                                     .delay(50)
